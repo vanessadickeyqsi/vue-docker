e8e20c533c5802d3efac9bed9d63a0ddda3d2301
Vanessa Dickey <vanessa.dickey@qsibanking.com>
2026-05-20 14:08:23 -0400

    Initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,12 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+    </template>
+    <template #heading></template>
+  </WelcomeItem>
+
+</template>
